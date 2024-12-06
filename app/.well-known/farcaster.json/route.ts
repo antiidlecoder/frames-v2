4f0743571f1e91289b20887034dd5b7761ba69f0
joel <joel.frehner@gmail.com>
2024-12-07 00:26:32 +0100

    test

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL;
-
+  //const appUrl = process.env.NEXT_PUBLIC_URL;
+  const appUrl = "https://frames-v2-theta.vercel.app";
   const config = {
     accountAssociation: {
       header:
